f18c94e41f037270913e233cacd258cbed34c3e7
Tom Wallis <github@tomwallis.net>
2018-10-24 14:40:07 +0100

    Some early changes to decisions, clever cascade decorator

--- workflow_graphs/workflow_graphs.py
+++ workflow_graphs/workflow_graphs.py
@@ -8,7 +8,14 @@ class WorkflowGraph(object):
         self.root = root
         self.current_executing_action = root
         self.action_adding_stack = []  # For building workflows.
-        self.decision_nesting_stack = []
+
+    @property
+    def currently_building_decision(self):
+        return type(self.action_adding_stack[-1]) == Decision
+
+    @property
+    def finished_executing(self):
+        return type(self.current_executing_action) == EndNode
 
     def run_workflow(self, actor, context=dict()):
         '''
@@ -21,10 +28,11 @@ class WorkflowGraph(object):
 
         self.current_executing_action = self.root
 
-        while self.current_executing_action.__class__ is not EndNode:
+        while not self.finished_executing:
             self.current_executing_action.execute(context, actor, WorkflowGraph.environment)
             self.current_executing_action = self.current_executing_action.next_action
 
+    @cascade
     def begin_with(self, first_action):
         if self.root is not None:
             raise BadWorkflowFormation("Already got somewhere to begin, but being given a second entrypoint! Conflicting\
@@ -32,8 +40,7 @@ class WorkflowGraph(object):
         self.root = first_action
         self.action_adding_stack.append(self.root)
 
-        return self
-
+    @cascade
     def then(self, next_action):
 
         # TODO
@@ -50,15 +57,63 @@ class WorkflowGraph(object):
 
         self.action_adding_stack.append(next_action)
 
-        return self
-
+    @cascade
     def decide_on(condition_function):
-        self.decision_building_stack.append(DecisionNode())
         pass
 
+    @cascade
     def when(cond):
         pass
 
+
+class Decision(object):
+    def __init__(self, condition):
+        self.condition = condition  # This should be a function which takes anything and returns a bool.
+        self.cases = {}
+        self.current_case = None
+        self.finished_construction = False
+
+    @property
+    def current_case_being_built(self):
+        return self.cases[self.current_case]
+
+    @cascade
+    def when(self, cond):
+        if cond in self.cases.keys():
+            raise BadWorkflowFormationException("Same case used to define two different workflows!")
+
+        #  If we're building a nested decision currently, pass this down to the one currently being built.
+        if self.current_case_being_built.currently_building_decision:
+            self.current_case_being_built.when(cond)
+
+        self.cases[cond] = WorkflowGraph()
+        self.current_case = None
+
+
+    @cascade
+    def then(self, next_action):
+        self.current_case_being_built.then(next_action)
+
+    @cascade
+    def decide_on(self, condition_function):
+        self.current_case_being_built.decide_on(next_action)
+
+    @cascade
+    def otherwise(self, next_action):
+        #  If we're building a nested decision currently, pass this down to the one currently being built.
+        if self.current_case_being_built.currently_building_decision:
+            self.current_case_being_built.otherwise(cond)
+
+        # TODO Define a default case
+        pass
+
+    @cascade
+    def join(self):
+        # TODO: What are the requirements for a decision being able to be joined?
+        # We need a default case
+        # Need to decide whether ending / joining is implicit at the end of a flow here (and which one if so), or whether
+        # they're *always* explicit. If explicit, are they also required?
+        self.construction_status = "finished"
 
 
 class Action(object):
@@ -97,9 +152,6 @@ class EndNode(object):
     pass
 
 
-class Decision(object):
-    pass
-
 
 class NoCurrentActionException(Exception):
     pass
@@ -107,6 +159,12 @@ class NoCurrentActionException(Exception):
 class BadWorkflowFormation(Exception):
     pass
 
+
+def cascade(method):
+    def _(*args, **kwargs):
+        method()
+        return method.im_self
+    return _
 
 def add_message_to_ctx(ctx, actor, env):
     ctx["first_message"] = "hello world!"
